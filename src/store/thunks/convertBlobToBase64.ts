b151a432166110ddf02adbfc2669e719dc364896
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2024-08-30 07:49:17 -0600

    Función para transformar de formato binario a un formato base64

---
+++ src/store/thunks/convertBlobToBase64.ts
@@ -0,0 +1,18 @@
+export const convertBlobToBase64 = (blob: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+  });
+};
+
+
+  export const fileToBase64 = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
+  };
