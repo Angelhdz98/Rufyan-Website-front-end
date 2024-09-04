cd6bd34188567e4cf88e92154afe8ac41f4d9d35
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2024-09-04 11:01:12 -0600

    Cambio del fetching para que funcione con la API y no  con Json server

--- src/store/thunks/fetchPaintings.ts
+++ src/store/thunks/fetchPaintings.ts
@@ -1,21 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { Product } from "../../types/typesIndex";
+import { Painting } from "../../types/typesIndex";
 
-
-const fetchPaintings= createAsyncThunk<Product[], void>('prodcucts/fetchPaintings', async () =>{
-    const response = await axios.get('http://localhost:3001/painting');
-    // dev only
-    await pause(1000);
-    return response.data.filter((product:Product )=> product.category ==='painting');
+const fetchPaintings= createAsyncThunk<Painting[], void>('prodcucts/fetchPaintings', async () =>{
+    const response = await axios.get('http://localhost::8080/paintings');
+    
+    
+    const data = response.data.map((paint:Painting)=>({
+        ...paint,
+        creation_date: 'fecha',
+    }));
+    
+    return  data;
 })
-const  pause = (duration:number) =>{
-    return new Promise((resolve)=>{
-    setTimeout(resolve, duration);
-    });
-    };
 
-//Dev only 
 
 
 
