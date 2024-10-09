262e0a1a559a01c5066840c92a269b071e73e2b2
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2024-10-09 12:07:31 -0600

    Nuevo tipo de boton y estilos

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,6 +12,7 @@ export interface buttonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
     outline?: boolean;
     rounded?: boolean;
     sorter?:boolean; 
+    no_Bg?:boolean;
 }
 
 
@@ -23,26 +24,28 @@ function Button ({children,
     danger,
     sorter,
     outline,
-    rounded, 
+    rounded,
+    no_Bg, 
     ...rest 
 }:buttonProps){
       
-const finalClassName = className(rest.className, 'flex items-center px-2 border border-[3px]', {
+const finalClassName = className('flex items-center px-2 border border-[3px]', rest.className, {
     "border-[#C04D2A] bg-[#EBAF9D]/[.70] text-[#8B351C] font-bold" : primary,
-    "border-[#D67254] bg-[#D67254] text-white font-bold" : secondary,
+    " border-[#D67254] bg-[#D67254] text-white font-bold " : secondary,
     "border-[#1D6720] bg-[#1D6720] text-white" : success,
     "border-yellow-500 bg-yellow-500 text-white" : warning,
+    "border-[#D67254]   text-[#D67254] font-bold ":no_Bg,
     "border-red-500 bg-red-500 text-white" : danger,
     "border-[#D67254] bg-[#b5b5b5] text-white":sorter,
     "rounded-xl": rounded,
-    "bg-white": outline,
+    " bg-white ": outline,
     "text-blue-500": outline && primary,
-    "text-gray-900": outline && secondary,
+    " text-[#D67254] ": outline && secondary,
     "text-green-500": outline && success,
     "text-yellow-500": outline && warning,
     "text-red-500": outline && danger,
 
-});
+}) ;
 
 
 
