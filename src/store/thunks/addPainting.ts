82b0566921db584f021b92937641b95698e47340
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2024-10-10 19:59:46 -0600

    Remover imports innecesarios

--- src/store/thunks/addPainting.ts
+++ src/store/thunks/addPainting.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 //import { Painting } from "../../types/typesIndex";
-import {  PaintingFormProps } from "../../pages/ControlPanel/AddPaintingForm";
 //import { PaintingsState } from "../slices/paintingsSlice";
 //import { Product } from "../../types/typesIndex";
 
