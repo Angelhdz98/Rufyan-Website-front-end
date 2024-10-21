2b8899d4fcfd40b8162c3e80d2b82d293a0712ab
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2024-10-21 13:37:59 -0600

    Cambios para volverlo más reutilizable y editable

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { HTMLAttributes, useEffect } from "react";
 import { FaWindowClose } from "react-icons/fa";
-interface ModalProps{
+interface ModalProps extends HTMLAttributes<HTMLDivElement>{
     isOpen: boolean;
     onClose: ()=> void;
     children: React.ReactNode;
+    divClassName?: string;
 }
-function Modal({isOpen, onClose, children}:ModalProps){
+function Modal({isOpen, onClose, divClassName, children, ...rest}:ModalProps){
     useEffect(()=>{
         if (isOpen){
             document.body.style.overflowY='hidden';
@@ -22,8 +23,8 @@ function Modal({isOpen, onClose, children}:ModalProps){
     if (!isOpen){
         return null;
     }else{
-        return(<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative bg-white shadow-lg rounded-lg w-2/3 h-2/3 overflow-y-auto p-4">
+        return(<div className={"fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 " + (divClassName? divClassName: '')}>
+            <div className={"relative bg-white shadow-lg rounded-lg w-2/3 h-2/3 overflow-y-auto p-4 " + rest.className}>
                 <button onClick={onClose} className="absolute top-[7px] right-2 text-gray-700 hover:text-gray-900">
                 <FaWindowClose className="text-[#D67254]"/>
                 </button>
