9d48d3e98aee1afed5deb12c110f8bee1183822d
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-03-10 22:49:21 -0600

    Changes to get a better general style

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom";
 import Header from "../components/Header";
 import DevBanner from "../components/DevBanner";
 export default function Root(){
-    return <div className="min-h-screen flex flex-col">
+    return <div className=" h-fit   flex flex-col">
         <Header/>
         <div className="flex-grow">
         <Outlet/>
