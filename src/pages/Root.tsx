b8393cf122d9824cc2330a2171815a73ad827254
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-04-30 22:45:01 -0600

    The Root component was edited to have a better view in all the pages

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,12 +2,15 @@ import {Outlet} from "react-router-dom";
 import Header from "../components/Header";
 import DevBanner from "../components/DevBanner";
 export default function Root(){
-    return <div className="  min-h-svh h-fit   flex flex-col">
+    return <div className=" min-h-svh h-fit  flex flex-col max-w-full relative ">
         <Header/>
-        <div className="flex-grow ">
-        <Outlet />
-        </div>
+
+    <div className=" flex-grow"><Outlet  />
+        </div>                
         <DevBanner/>
+        
+        
+         
         
     </div>
 }
