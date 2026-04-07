d42100bc5a5089141687aaf94c676d4f4baf485f
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-04-07 15:02:39 -0600

    Refactor InfoChart and SwiperSlider components for improved clarity and functionality

--- src/components/InfoChart.tsx
+++ src/components/InfoChart.tsx
@@ -1,63 +1,76 @@
 import { useState } from "react";
+import { CartItemInterface, Cup, Hat, Painting, PaintingPricing, SinglePricing } from "../types/typesIndex";
 import AddressChart from "./AddressChart";
 import Button from "./Button";
-import { CartItemInterface, Painting, Hat, Cup, isPainting } from "../types/typesIndex";
 
-export interface InfoChartProps{
+export interface InfoChartProps {
   items: (CartItemInterface<Painting> | CartItemInterface<Hat> | CartItemInterface<Cup>)[]
 }
 
 
-function InfoChart({items}:InfoChartProps){
-    
+function InfoChart({ items }: InfoChartProps) {
+
   const [isInForm, setIsInForm] = useState(false);
- const changeIsInForm = (value: boolean) =>{
+  const changeIsInForm = (value: boolean) => {
     setIsInForm(value);
   }
-  let precio =0; 
-    items.forEach((item)=>{
-      
-      if(item.isCopy && isPainting(item.product)){
-        precio= precio+item.product.price_copy;
-      }
-      else if(item.isCopy! && isPainting(item.product)){
-        precio=precio+ item.product.price; 
-      }else{
-        precio= precio+item.product.price;
-      }
-      
-    })
-    const shipmentPrice= 250;
-  
-    return <div className=" flex flex-row "> 
-                <div className=" flex flex-col ">
-                   <span className="font-bold px-2"> Address</span>
-                 <AddressChart isInForm={isInForm} changeIsInForm={changeIsInForm} />
-                  <div className={isInForm? " hidden": " "}>
-                     <div className=" flex flex-col ">
-                        <div className="flex flex-row justify-between mx-2 "> 
-                                <span className="font-medium">Products price: </span> <span> {precio} 00MXN</span>
-                        </div>
-                        <div className="flex flex-row justify-between mx-2 "> 
-                                <span className="font-medium">Shipment price: </span> <span> {shipmentPrice}.00MXN</span>
-                        </div>
-                    </div>
+  let precio: number = 0;
+  items.forEach((item) => {
+    // aun falta el precio no esta correcto 
 
-                <div className="subtotal-price flex flex-row justify-end my-4 px-4 gap-12">
-              <Button primary rounded className="text-sm w-1/3">
-                Proceed to payment
-              </Button>
-              <div>
-                <span className="font-semibold ">Total: {shipmentPrice+precio}.00MXN</span>
-              </div>
-            </div>
-                  </div>
-                   
-                
+    switch (item.product.productPricing.pricingType) {
+      case "ORIGINAL": {
+        const paintingPrice = item.product.productPricing as PaintingPricing;
+        if (item.isCopy) {
+          precio = precio + paintingPrice.pricePerCopy;
+        }
+        else {
+          precio = precio + paintingPrice.pricePerOriginal
+        }
+        break;
+      }
 
-                </div>
-                
-           </div>
+      case "SINGLE_PRICE": {
+        const singlePricing = item.product.productPricing as SinglePricing;
+        precio = precio + singlePricing.price;
+        break;
+      }
+
+    }
+
+
+  });
+  const shipmentPrice = 250;
+
+  return <div className=" flex flex-row ">
+    <div className=" flex flex-col ">
+      <span className="font-bold px-2"> Address</span>
+      <AddressChart isInForm={isInForm} changeIsInForm={changeIsInForm} />
+      <div className={isInForm ? " hidden" : " "}>
+        <div className=" flex flex-col ">
+          <div className="flex flex-row justify-between mx-2 ">
+            <span className="font-medium">Products price: </span> <span> {precio} 00MXN</span>
+          </div>
+          <div className="flex flex-row justify-between mx-2 ">
+            <span className="font-medium">Shipment price: </span> <span> {shipmentPrice}.00MXN</span>
+          </div>
+        </div>
+
+        <div className="subtotal-price flex flex-row justify-end my-4 px-4 gap-12">
+          <Button primary rounded className="text-sm w-1/3">
+            Proceed to payment
+          </Button>
+          <div>
+            <span className="font-semibold ">Total: {shipmentPrice + precio}.00MXN</span>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+
+  </div>
 }
 
 export default InfoChart;
--- src/components/SwiperSlider.tsx
+++ src/components/SwiperSlider.tsx
@@ -1,160 +1,165 @@
+import { EffectCube, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, EffectCube} from "swiper/modules";
 
 
-import "swiper/css/navigation"
-import "swiper/css"
-import "swiper/css/effect-cards"
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "swiper/css/navigation";
 
 
- //  importamos los estilos para el lazy loading
+//  importamos los estilos para el lazy loading
 
 
 
-import { ImageProduct, isPainting, Product } from "../types/typesIndex";
 import { HTMLAttributes, useState } from "react";
-import { IoIosCheckmarkCircle } from "react-icons/io";
-import { CiNoWaitingSign } from "react-icons/ci";
+import { ImageProduct, Product } from "../types/typesIndex";
+//import { IoIosCheckmarkCircle } from "react-icons/io";
+//import { CiNoWaitingSign } from "react-icons/ci";
 import { LikeButton } from "./LikeButton";
+import PriceTag from "./PriceTag";
+import StockTag from "./StockTag";
 
-interface SwiperSliderProps extends HTMLAttributes<HTMLDivElement>{
-    images: ImageProduct[]; 
-    product?:Product;
-    className?:string;
-};
+interface SwiperSliderProps extends HTMLAttributes<HTMLDivElement> {
+    images: ImageProduct[];
+    product: Product;
+    className?: string;
+}
 
 
-function SwiperSlider(props:SwiperSliderProps){
+function SwiperSlider(props: SwiperSliderProps) {
 
     const [selectedIndex, setSelectedIndex] = useState(0);
     const [loaded, setLoaded] = useState(false);
-    const [isOriginalSelected, SetIsOriginalSelected]= useState(false);
-    
+    //const [isOriginalSelected, SetIsOriginalSelected]= useState(false);
 
-    const nextSlide = ()=>{
+
+    const nextSlide = () => {
         setLoaded(false);
 
-        if(selectedIndex < props.images.length-1){
-            
-            setSelectedIndex(selectedIndex+1);
-            console.log(props.images.length+"  " + selectedIndex);
-        }else if (selectedIndex == props.images.length-1) {
-           
+        if (selectedIndex < props.images.length - 1) {
+
+            setSelectedIndex(selectedIndex + 1);
+            console.log(props.images.length + "  " + selectedIndex);
+        } else if (selectedIndex == props.images.length - 1) {
+
             setSelectedIndex(0);
             console.log("else if activado");
 
         }
-        console.log("indice seleccionado por next "+ selectedIndex);
+        console.log("indice seleccionado por next " + selectedIndex);
     }
-    const prevSlide = ()=>{
+    const prevSlide = () => {
         setLoaded(false);
 
-        if(selectedIndex == 0){
-            setSelectedIndex(props.images.length-1);
-        }else {
-            setSelectedIndex(selectedIndex-1);
+        if (selectedIndex == 0) {
+            setSelectedIndex(props.images.length - 1);
+        } else {
+            setSelectedIndex(selectedIndex - 1);
         }
-        console.log("indice seleccionado por prev"+selectedIndex);
-        
+        console.log("indice seleccionado por prev" + selectedIndex);
+
     }
 
-    const loadedHandler = () =>{
+    const loadedHandler = () => {
         setLoaded(true);
-        console.log(" Indice seleccionado tra finalizar la carga " +selectedIndex);
+        console.log(" Indice seleccionado tra finalizar la carga " + selectedIndex);
     }
-    
+    /*
     const priceTag = <div onClick={()=>SetIsOriginalSelected(!isOriginalSelected)}
     className="absolute top-2 px-1 right-4 z-10 bg-white/70 rounded-md">
         {(props.product && isPainting(props.product)&& isOriginalSelected  )? <span>Price original: {props.product.price}</span>:(props.product && isPainting(props.product)&& !isOriginalSelected  )? <span>Price copy: {props.product.price_copy} </span>: <span>Price product: {props.product?.price}</span> }
     </div>
+    */
 
-const availability = ()=>{
-    if(props.product)
-    if(isPainting(props.product)&& props.product.isOrginalAvailable){
-        return <div className="flex">
-        Original
-        <IoIosCheckmarkCircle className="text-green-500 mt-1" />
-    </div> 
-    }
-    else if(isPainting(props.product)&&!props.product.isOrginalAvailable&& props.product.available_copies>0){
-        return <div className="flex items-center" >
-        Original
-        <CiNoWaitingSign className="text-red-500 stroke-2 " />
-    </div>
 
-    } else if(!isPainting(props.product)&& props.product.available){
-        return <div className="flex items-center" >
-        Original
-        <CiNoWaitingSign className="text-red-500 stroke-2 " /> 
+    /*
+    const availability = ()=>{
+        if(props.product)
+        if(isPainting(props.product)&& props.product.isOrginalAvailable){
+            return <div className="flex">
+            Original
+            <IoIosCheckmarkCircle className="text-green-500 mt-1" />
+        </div> 
+        }
+        else if(isPainting(props.product)&&!props.product.isOrginalAvailable&& props.product.available_copies>0){
+            return <div className="flex items-center" >
+            Original
+            <CiNoWaitingSign className="text-red-500 stroke-2 " />
         </div>
-    } else{
-        return <div className="font-bold text-red-500">
-        Sold out
-    </div>
+    
+        } else if(!isPainting(props.product)&& props.product.available){
+            return <div className="flex items-center" >
+            Original
+            <CiNoWaitingSign className="text-red-500 stroke-2 " /> 
+            </div>
+        } else{
+            return <div className="font-bold text-red-500">
+            Sold out
+        </div>
+        }
     }
-}
+    */
 
-    const availabilityTag = <div className="flex gap-2 text-sm  original-available-tag absolute items-center z-10 bg-white/70 rounded top-2 left-4 px-1 ">{availability()}
 
-    </div>;
-         
-         const availableCopies = ()=>{
-            if(props.product)
+
+
+    /*
+    const availableCopies = ()=>{
+       if(props.product)
 return isPainting(props.product)&& props.product.available_copies > 0 ?
-        <div className="Available-copies text-xs bg-white/70 absolute z-10 left-3 rounded px-1  bottom-2">Copies: {props.product.available_copies}/{props.product.copies_made}</div> :
-        <div className="z-10  text-red-500 font-bold text-xs bg-white/70 absolute left-3 rounded px-1  bottom-2">No copies available</div>
-         } 
+   <di v className="Available-copies text-xs bg-white/70 absolute z-10 left-3 rounded px-1  bottom-2">Copies: {props.product.available_copies}/{props.product.copies_made}</div> :
+   <div className="z-10  text-red-500 font-bold text-xs bg-white/70 absolute left-3 rounded px-1  bottom-2">No copies available</div>
+    } 
+    */
 
-  /*
-  const liked = ()=>{
-    return <div className="text-[#eb4b1b] rounded-md text-3xl absolute right-4 bottom-2 border bg-white/70  z-10 ">
-    {isProductLiked? <FaHeart onClick={() =>{setIsProductLiked(false)}} /> :<FaRegHeart  onClick={()=>setIsProductLiked(true)}/>}
-  </div>
-  
- }   
-  */
+    /*
+    const liked = ()=>{
+      return <div className="text-[#eb4b1b] rounded-md text-3xl absolute right-4 bottom-2 border bg-white/70  z-10 ">
+      {isProductLiked? <FaHeart onClick={() =>{setIsProductLiked(false)}} /> :<FaRegHeart  onClick={()=>setIsProductLiked(true)}/>}
+    </div>
+    
+   }   
+    */
 
 
-    const slides= props.images.map((image, index)=>{
-        return <SwiperSlide   key={index}   className="h-auto  ">
+    const slides = props.images.map((image, index) => {
+        return <SwiperSlide key={index} className="h-auto  ">
             {/***object-cover h-full  */}
             {//SRC para conectar con la API
             /*src={`http://localhost:8080${image.url}`}*/}
-            <img   src={image.url} 
-                    alt={image.productName}
-                    onClick={props.onClick}
-                    loading="lazy"
-                    onLoadedData={() =>loadedHandler()}
-                    className={(selectedIndex==index?"":"hidden ") + (loaded?" opacity-100 " : "  ") +" object-cover  h-full   " +props.className} />
-                    <div className={"swiper-lazy-preloader swiper-lazy-preloader-white "}></div>
-            
-            </SwiperSlide>
+            <img src={image.url}
+                alt={image.productName}
+                onClick={props.onClick}
+                loading="lazy"
+                onLoadedData={() => loadedHandler()}
+                className={(selectedIndex == index ? "" : "hidden ") + (loaded ? " opacity-100 " : "  ") + " object-cover  h-full   " + props.className} />
+            <div className={"swiper-lazy-preloader swiper-lazy-preloader-white "}></div>
+
+        </SwiperSlide>
     })
 
     return <Swiper
-                 onSlideNextTransitionStart={nextSlide}
-                 onSlidePrevTransitionStart={prevSlide}
-                navigation 
-                effect="cards"  loop 
-                modules={[Navigation, EffectCube]}
-                watchSlidesProgress
-                lazyPreloadPrevNext={0}
-                className={props.className}
-                
-                
+        onSlideNextTransitionStart={nextSlide}
+        onSlidePrevTransitionStart={prevSlide}
+        navigation
+        effect="cards" loop
+        modules={[Navigation, EffectCube]}
+        watchSlidesProgress
+        lazyPreloadPrevNext={0}
+        className={props.className}
+    >
 
+        <PriceTag productPricing={props.product.productPricing}></PriceTag>
 
-                >  
-                {priceTag}   
-                {availabilityTag}  
-                {availableCopies()}
-                <div className= {`text-[#eb4b1b] rounded-md text-3xl absolute z-10 bottom-2 right-2   border bg-white/70  cursor-pointer `} >
-                <LikeButton/>   
-                </div>
-                
+        <StockTag productStock={props.product.productStock} />
+
+        <div className={`text-[#eb4b1b] rounded-md text-3xl absolute z-10 bottom-2 right-2   border bg-white/70  cursor-pointer `} >
+            <LikeButton />
+        </div>
+
         {slides}
-       
-    </Swiper> 
+
+    </Swiper>
 
 }
 
