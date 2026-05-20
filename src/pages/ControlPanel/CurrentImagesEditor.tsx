4f27fb5587ef96e907973cb71d9536658d0a3ed5
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-20 12:25:24 -0600

    current image editor works

--- src/pages/ControlPanel/CurrentImagesEditor.tsx
+++ src/pages/ControlPanel/CurrentImagesEditor.tsx
@@ -1,31 +1,35 @@
-import { HTMLAttributes, useEffect } from "react";
+import { HTMLAttributes} from "react";
 import { ImageProduct } from "../../types/typesIndex";
 import UploadedImagePreview from "../../components/UploadedImagePreview";
-import { useProductForm } from "./useProducForm";
+
 
 interface CurrentImagesEditorProps extends HTMLAttributes<HTMLDivElement> {
 
-    images: ImageProduct[];
     imageChartClassName?: string;
+    setCurrentImages: (images: ImageProduct[]) => void;
+    currentImages?: ImageProduct[];
 }
 
-function CurrentImagesEditor(props: CurrentImagesEditorProps) {
+function    CurrentImagesEditor(props: CurrentImagesEditorProps) {
 
 
-    const { currentImages, setCurrentImages } = useProductForm();
 
-    useEffect(() => {
-        setCurrentImages(props.images);
-    }, [props.images])
 
+    /*    useEffect(() => {
+            setCurrentImages(props.images);
+        }, [props.images])
+    */
 
     const deleteImage = (imageId: number) => {
-        const updatedImages = currentImages.filter((image) => image.id !== imageId);
-        console.log("imagenes actualizadas: ", updatedImages)
-        setCurrentImages(updatedImages);
+        if (props.currentImages) {
+            const updatedImages = props.currentImages.filter((image) => image.id !== imageId);
+            console.log("imagenes actualizadas: ", updatedImages)
+            props.setCurrentImages(updatedImages);
+        }
+
     }
 
-    const renderedImages = currentImages.map((img: ImageProduct) => {
+    const renderedImages = props.currentImages == null ? "" : props.currentImages.map((img: ImageProduct) => {
         return <UploadedImagePreview onClick={() => {
             console.log("imagen a eliminar:" + img.id)
             deleteImage(img.id)
