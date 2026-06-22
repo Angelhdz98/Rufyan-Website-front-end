c5ee763029cc22d8ddbb5da1de5b485c1fcfd4aa
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-06-21 22:07:39 -0600

    feat: enhance sorting functionality in NavProduct and Sorter components

--- src/components/NavProduct.tsx
+++ src/components/NavProduct.tsx
@@ -1,22 +1,44 @@
+import { useContext } from "react";
+import { ProductsEditingContext } from "../pages/ControlPanel/useEditProductsContext";
 import CategorySelector from "./CategorySelector";
 import SearchBar from "./SearchBar";
-import Sorter from "./Sorter";
+import Sorter, { SorterTypeEnum, SortOrderEnum } from "./Sorter";
+import { handleGetPagedProducts } from "./ProductRequests";
+import { Page } from "../types/typesIndex";
+import mapBackendProductToFrontend from "../pages/ControlPanel/ProductBackendMapper";
 
 
-function NavProduct(){
+function NavProduct() {
+
+    const productsContext = useContext(ProductsEditingContext);
+
+    const sortChange = (sortBy: SorterTypeEnum, pageNumber: number, order: SortOrderEnum) => {
+
+        const response = handleGetPagedProducts(
+            productsContext?.sortType ? productsContext?.sortType : SorterTypeEnum.CREATION_DATE,
+            productsContext?.pageNumber ? productsContext?.pageNumber : 0,
+            productsContext?.pageSize ? productsContext?.pageSize : 12,
+            productsContext?.sortOrder ? productsContext?.sortOrder : SortOrderEnum.DESCENDING).then((products: Page<any>) => {
+
+                const mappedProducts = products.content.map(mapBackendProductToFrontend);
+                productsContext?.setProducts(mappedProducts);
+            });
+
+    };
 
 
+    return <div className="flex flex-col gap-4 w-full items-start my-3 h-fit  ">
 
-    return <div className="flex flex-col gap-4 w-full items-start my-3 h-fit ">
         <div className=" flex flex-row max-h-7 w-full">
-        <CategorySelector/>
-        <SearchBar className="md:w-4/5 lg:w-9/10 h-6"/>
+
+            <CategorySelector />
+            <SearchBar className="md:w-4/5 lg:w-9/10 h-6" />
         </div>
-        
+
 
         <div className="flex flex-col gap-1 w-full px-12 ">
-           
-            <Sorter/>
+
+            <Sorter onChange={sortChange} />
         </div>
     </div>
 }
