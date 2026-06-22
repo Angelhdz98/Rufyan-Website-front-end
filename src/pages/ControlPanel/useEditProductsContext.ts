a9abf3ce14db187e196820486d9a2206a9dbe544
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-06-21 22:12:25 -0600

    feat: add ProductsEditingContext for managing product editing state

---
+++ src/pages/ControlPanel/useEditProductsContext.ts
@@ -0,0 +1,33 @@
+import { createContext } from "react";
+import { Product } from "../../types/typesIndex";
+import { SorterTypeEnum, SortOrderEnum } from "../../components/Sorter";
+
+
+export interface ProductEditingContextProp {
+    products?: Product[],
+    setProducts: React.Dispatch<React.SetStateAction<Product[]>>,
+    pageNumber?: number,
+    setPageNumber: React.Dispatch<React.SetStateAction<number>>,
+    pageSize?: number,
+    setPageSize: React.Dispatch<React.SetStateAction<number>>,
+    sortOrder?: SortOrderEnum,
+    setSortOrder: React.Dispatch<React.SetStateAction<SortOrderEnum>>,
+    sortType?: SorterTypeEnum,
+    setSortType: React.Dispatch<React.SetStateAction<SorterTypeEnum>>,
+    searchTerm?: string,
+    setSearchTerm: React.Dispatch<React.SetStateAction<string | undefined>>,
+    //charging Flag
+    isLoading?: boolean,
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+    error?: string,
+    setError: React.Dispatch<React.SetStateAction<string | undefined>>
+
+
+
+
+}
+
+export const ProductsEditingContext = createContext<ProductEditingContextProp | undefined>(undefined);
+
+
+
