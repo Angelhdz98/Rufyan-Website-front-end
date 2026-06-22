c491aeba230c485f8645aa50ffddf09e79ff37b3
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-06-21 22:08:29 -0600

    refactor: update SorterButton to use SortOrderEnum for sort order checks

--- src/components/SorterButton.tsx
+++ src/components/SorterButton.tsx
@@ -1,30 +1,28 @@
-
-
-import { useSelector } from "react-redux";
 import Button from "./Button"
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
-import { RootState } from "../store";
+import { SortOrderEnum } from "./Sorter";
 
 interface SorterButtonProps {
     children: string;
-    sortBy: string;
     onClick: () => void;
     isSorted: boolean;
+    sortOrder: SortOrderEnum;
+
 };
 
 function SorterButton(props: SorterButtonProps) {
 
-    const { sortOrder } = useSelector((state: RootState) => state.sortProducts)
+    //const { sortOrder } = useSelector((state: RootState) => state.sortProducts)
     return <Button onClick={props.onClick} rounded sorter className="flex flex-row gap-3 max-h-8 px-2 py-0 ">
         {props.children}
         <div className="flex flex-col text-[#E88769]">
             {
-                (props.isSorted && sortOrder == 'desc') ?
+                (props.isSorted && props.sortOrder == SortOrderEnum.DESCENDING) ?
                     <FaArrowUp className="text-xs opacity-0 " /> :
                     <FaArrowUp className="text-xs opacity-100 " />
             }
             {
-                (props.isSorted && sortOrder == 'asc') ?
+                (props.isSorted && props.sortOrder == SortOrderEnum.ASCENDING) ?
                     <FaArrowDown className="text-xs opacity-0" /> :
                     <FaArrowDown className="text-xs opacity-100" />
             }
