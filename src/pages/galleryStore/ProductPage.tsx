0e1967e932c6dd99038d7f41b142934351a66ddf
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-08-08 14:39:00 -0600

    Product page done

--- src/pages/galleryStore/ProductPage.tsx
+++ src/pages/galleryStore/ProductPage.tsx
@@ -1,6 +1,11 @@
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "swiper/css/navigation";
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-//import ImageSwiper from "../../components/ImageSwiper";
 import OriginalSelector from "./OriginalSelector";
 
 import {
@@ -10,9 +15,9 @@ import {
     ProductTypeEnum,
 } from "../../types/typesIndex";
 import { getProductByIdRequest } from "../../components/ProductRequests";
-import ImageSwiper from "../../components/ImageSwiper";
-import { mapProductDTOToProduct } from "../ControlPanel/ProductBackendMapper";
-//import { mapProductDTOToProduct } from "../ControlPanel/ProductBackendMapper";
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { CiNoWaitingSign } from "react-icons/ci";
+import { LikeButton } from "../../components/LikeButton";
 
 function ProductPage() {
     const { id } = useParams();
@@ -46,12 +51,49 @@ function ProductPage() {
         fetchProduct();
     }, [id]);
 
+    const slides = product?.images?.map((image, index) => (
+        <SwiperSlide key={`${image.url}-${index}`} className="h-auto">
+            <img
+                src={image.url}
+                alt={image.productName}
+                loading="lazy"
+                className="w-full h-full object-cover"
+            />
+
+        </SwiperSlide>
+    ));
+
+    const originalAvailableTag = () => {
+        if (product) {
+            const paintingStock = product.productStockDTO as PaintingStock;
+            if (paintingStock.isOriginalAvailable) {
+                return <div className="flex">
+                    Original
+                    <IoIosCheckmarkCircle className="text-green-500 mt-1" />
+                </div>
+            } else if (paintingStock.stockCopies > 0) {
+                return <div className="flex items-center" >
+                    Original
+                    <CiNoWaitingSign className="text-red-500 stroke-2 " />
+                </div>
+            } else {
+                return <div className="font-bold text-red-500">
+                    Sold out
+                </div>;
+
+            }
+
+        } else return <div>no Product</div>
+
+    }
+
     const renderProperties = () => {
         if (!product) return null;
 
         if (product.productTypeEnum === ProductTypeEnum.PAINTING) {
             const details = product.productDetails as PaintingDomainDetails;
             const stock = product.productStockDTO as PaintingStock;
+
             return (
                 <div className="flex flex-col w-full px-4 gap-1">
                     <div>
@@ -89,19 +131,48 @@ function ProductPage() {
     }
 
     return (
-        <div className="p-2">
-            <ImageSwiper product={mapProductDTOToProduct(product)} title={product.name}/> 
-            <div>
-                <span className="font-semibold">Description:</span>{" "}
-                {product.description}
-            </div>
-            <div>
-                <span className="font-semibold">Category:</span>{" "}
-                {product.productTypeEnum}
+        <div className="w-full max-w-full overflow-x-hidden p-4 flex flex-col  md:flex-row md:flex-wrap sm:justify-center md:justify-center gap-4">
+            <div className=" w-full md:w-[45%] lg:w-[40%] min-w-0 mx-auto    flex flex-col items-center   ">
+                <div className="   max-w-full  sm:max-w-[70%]   ">
+                    <Swiper
+                        navigation
+                        effect="slide"
+                        loop
+                        modules={[Navigation]}
+                        className="rounded-t-lg overflow-hidden  "
+                    >
+                        {slides}
+                        <div className="flex gap-2 text-sm  original-available-tag absolute items-center z-10 bg-white/70 rounded top-2 left-4 px-1 ">
+                            {originalAvailableTag()}
+                        </div>
+                        <div className={`text-[#eb4b1b] rounded-md text-3xl absolute z-10 bottom-12 right-4   border bg-white/70  cursor-pointer scale-[1.5] `} >
+                            <LikeButton productId={product.id} />
+                        </div>
+
+                    </Swiper>
+
+                    <div className="border-b-4 border-x-4 rounded-b-md border-black">
+                        <span className="block p-2">{product.name}</span>
+                    </div>
+
+                    <div className="px-3">
+                        <div className="mt-4 w-full break-words   ">
+                            <span className="font-semibold">Description:</span>{" "}
+                            {product.description}
+                        </div>
+
+                        <div className="mt-2 w-auto break-words">
+                            <span className="font-semibold">Category:</span>{" "}
+                            {product.productTypeEnum}
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
 
-            <div className="md:w-8/12 flex flex-col gap-4">
-                <OriginalSelector />
+            <div className="w-full md:flex-1 min-w-0 flex flex-col gap-4">
+                <OriginalSelector product={product} />
                 {renderProperties()}
             </div>
         </div>
