b362576fbe4037a0fb528d9995957588f6b650d9
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-08-10 16:08:11 -0600

    making request for liked products

--- src/pages/LikedPage/UserLikes.tsx
+++ src/pages/LikedPage/UserLikes.tsx
@@ -1,12 +1,69 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
 import FavoriteProduct from "../../components/FavoriteProduct";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Masonry from "react-masonry-css";
+import { getAccessToken } from "../ControlPanel/authStore";
+import { jwtDecode } from "jwt-decode";
+import { ProductDTO, TokenPayload } from "../../types/typesIndex";
+import { getLikedProducts } from "../../components/ProductRequests";
+import { mapProductDTOToProduct } from "../ControlPanel/ProductBackendMapper";
 
 function UserLikes() {
 
-  const { data, isLoading } = useSelector((state: RootState) => state.paintings);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [pageNumber] = useState(0);
+  const [productsDTO, setProductsDTO] = useState<ProductDTO[]>([]);
+
+
+  const checkAuth = () => {
+    const accessToken = getAccessToken();
+
+    if (!accessToken) {
+      setIsAdmin(false);
+      setIsAuthenticated(false);
+      return;
+    }
+
+    try {
+      const jwt = jwtDecode<TokenPayload>(accessToken);
+      /*                console.log("Token decodificado en PrivateRoute:", jwt);
+                      console.log("Rol:", jwt.role);
+      */
+      // Verificar si el rol es ROLE_ADMIN
+      if (jwt.role === 'ROLE_ADMIN') {
+        setIsAdmin(true);
+        setIsAuthenticated(true);
+
+      } else {
+        setIsAdmin(false);
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      console.error("Error decodificando token:" + error);
+      setIsAdmin(false);
+    }
+  };
+
+
+  useEffect(() => {
+    checkAuth();
+    if (isAuthenticated) {
+      if (isAdmin) { alert("Los likes son solo para los usuarios los productos favoritos estan la pagina principal"); }
+      else {
+
+        getLikedProducts(pageNumber).then((response) => {
+          setProductsDTO(response.content);
+        }).catch((error) => {
+          alert("Hubo un error pidiendo la obra:  " + error);
+        }).finally(() => {
+          setIsLoading(false);
+        });
+      }
+    }
+
+  }, [pageNumber, isAuthenticated, isAdmin]);
+
+  const [isLoading, setIsLoading] = useState(true);
   const [visibleLabels, setVisibleLabels] = useState<Record<number, boolean>>({})
   const [clickedPaints, setClickedPaints] = useState<Record<number, boolean>>({});
   const handleMouseEnter = (id: number) => {
@@ -21,12 +78,12 @@ function UserLikes() {
     setClickedPaints((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
-  const renderedFavPaints = data.map((fp) => {
+  const renderedFavPaints = productsDTO.map((fp) => {
     const isVisible = (visibleLabels[fp.id] || clickedPaints[fp.id]);
     return <FavoriteProduct
 
       key={fp.id}
-      product={fp}
+      product={mapProductDTOToProduct(fp)}
       clicked={clickedPaints[fp.id]}
       isVisible={isVisible}
       onMouseEnter={() => handleMouseEnter(fp.id)}
