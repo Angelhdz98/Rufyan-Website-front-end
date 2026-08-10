3df8821ebcab1953b09fde127b655e3b3b348c96
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-08-10 14:07:31 -0600

    Add favorite product functionality to LikeButton component and related requests

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,6 +1,10 @@
 import { Fragment, useEffect, useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { isProductLikedRequest, markAsLikedRequest, unmarkAsLikedRequest } from "./UserRequest";
+import { getAccessToken } from "../pages/ControlPanel/authStore";
+import { jwtDecode } from "jwt-decode";
+import { TokenPayload } from "../types/typesIndex";
+import { isProductFavoriteByIdRequest, markProductAsFavoriteRequest, unmarkProductAsFavoriteRequest } from "./ProductRequests";
 
 
 /*interface LikeButtonProps extends HtmlHTMLAttributes<HTMLDivElement>{
@@ -13,48 +17,121 @@ export function LikeButton(props: LikeButtonProps) {
   const [isProductLiked, setIsProductLiked] = useState(false);
   //const productData = useContext(ProductContext);
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
+
+
+
+  const checkAuth = () => {
+    const accessToken = getAccessToken();
+
+    if (!accessToken) {
+      setIsAdmin(false);
+      setIsAuthenticated(false);
+      return;
+    }
+
+    try {
+      const jwt = jwtDecode<TokenPayload>(accessToken);
+      /*                console.log("Token decodificado en PrivateRoute:", jwt);
+                      console.log("Rol:", jwt.role);
+      */
+      // Verificar si el rol es ROLE_ADMIN
+      if (jwt.role === 'ROLE_ADMIN') {
+        setIsAdmin(true);
+        setIsAuthenticated(true);
+
+      } else {
+        setIsAdmin(false);
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      console.error("Error decodificando token:" + error);
+      setIsAdmin(false);
+    }
+  };
 
 
   useEffect(() => {
+    checkAuth();
 
-    if (props.productId)
-      isProductLikedRequest(props.productId).then((isLikedByUser) => {
-        setIsProductLiked(isLikedByUser);
-      }).catch((error) => {
-        alert("Hubo un error buscando el like: " + error);
-      });
+    if (props.productId) {
+
+      if (isAuthenticated) {
+        //if User is admin component shows if Product is favorite  
+        if (isAdmin) {
+          isProductFavoriteByIdRequest(props.productId).then((isFavorite) => {
+            setIsProductLiked(isFavorite);
+          }).catch((error) => {
+            alert("Ocurrió un error checando si la obra es favorita: " + error);
+          });
+        } else {
+          //if is authenticated but is not admin will show if product is liked
+          isProductLikedRequest(props.productId).then((isLikedByUser) => {
+            setIsProductLiked(isLikedByUser);
+          }).catch((error) => {
+            alert("Hubo un error buscando el like: " + error);
+          });
+        }
+
+      }
 
 
 
-  }, []);
+    }
+
+
+
+  }, [props.productId, isAuthenticated, isAdmin]);
 
   const clickLikeHandler = () => {
     if (props.productId) {
-      markAsLikedRequest(props.productId).then(response => {
-        if (response) {
+      if (isAdmin) {
+        markProductAsFavoriteRequest(props.productId).then((response) => {
           setIsProductLiked(response);
-        } else {
-          alert("hubo un error desconocido");
-        }
+        }).catch((error) => {
+          alert("Hubo un error marcando la obra como favorita " + error);
+        });
+      } else {
+        markAsLikedRequest(props.productId).then(response => {
+          if (response) {
+            setIsProductLiked(response);
+          } else {
+            alert("hubo un error desconocido");
+          }
 
-      }).catch((error) => {
-        alert("Hubo un error: " + error);
-      });
+        }).catch((error) => {
+          alert("Hubo un error: " + error);
+        });
+      }
     } else return;
   };
 
   const clickUnlikeHandler = () => {
 
     if (props.productId) {
-      unmarkAsLikedRequest(props.productId).then((response) => {
-        if (response == false) {
-          alert("Hubo un error desconocido")
-        }
-        setIsProductLiked(!response);
-      }).catch((error) => {
-        alert("Hubo un error: " + error);
-      });
+      if (isAdmin) {
+        unmarkProductAsFavoriteRequest(props.productId)
+          .then(() => {
+            setIsProductLiked(false);
+          })
+          .catch((error) => {
+            alert("Hubo un error desmarcando la obra como favorita: " + error);
+          });
+      } else {
+
+        unmarkAsLikedRequest(props.productId)
+          .then((response) => {
+            if (response == false) {
+              alert("Hubo un error desconocido")
+            }
+            setIsProductLiked(!response);
+          })
+          .catch((error) => {
+            alert("Hubo un error: " + error);
+          });
+      }
     } else {
       alert("No product selected");
     }
@@ -66,7 +143,7 @@ export function LikeButton(props: LikeButtonProps) {
 
   const likedHeart = <FaHeart onClick={clickUnlikeHandler} />
   const noLikedHeart = <FaRegHeart onClick={clickLikeHandler} />
-  return <Fragment   >
+  return isAuthenticated ? <Fragment   >
     {isProductLiked ? likedHeart : noLikedHeart}
-  </Fragment>
+  </Fragment> : ""
 }
