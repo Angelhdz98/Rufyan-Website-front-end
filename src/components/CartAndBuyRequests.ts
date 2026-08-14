35591fa9454f3391968b1d745edbffe3d4aeaa07
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-08-14 16:50:55 -0600

    Fix removeCartItemRequest to use cartItemId instead of productId

--- src/components/CartAndBuyRequests.ts
+++ src/components/CartAndBuyRequests.ts
@@ -23,11 +23,23 @@ export const addCartItemRequest = async (productId:number, cartItemDetails:CartI
 }
 
 
-export const removeCartItemRequest = async (productId: number)=>{
+export const removeCartItemRequest = async (cartItemId: number)=>{
 
-    const response = await api.put("/shopping-cart/remove-item/"+productId);
+    const response = await api.put("/shopping-cart/remove-item/"+cartItemId);
 
 if(response.status !== 200 && response.status !== 201){
+        throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return response.data as ShoppingCartDTO; 
+
+
+}
+
+export const getCartItems = async () =>{
+    
+    const response  = await api.get("/shopping-cart"); 
+    if(response.status !== 200 && response.status !== 201){
         throw new Error(`Error: ${response.statusText}`);
     }
 
