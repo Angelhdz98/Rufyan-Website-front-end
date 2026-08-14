37cc46ba32699368b019a065bc32a57190a134be
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-08-14 17:00:26 -0600

    Refactor Cart and CartItem components to enhance item management and display total price

--- src/pages/cartPage/CartItem.tsx
+++ src/pages/cartPage/CartItem.tsx
@@ -1,46 +1,38 @@
 
 import Button from "../../components/Button";
 import PiecesSelector from "../../components/PiecesSelector";
-import PriceTag from "../../components/PriceTag";
-import { Product } from "../../types/typesIndex";
-import { isPainting } from "../../types/typesIndex";
+import { CartItemDTORecord, PaintingItemDetails, ProductTypeEnum } from "../../types/typesIndex";
+
 interface cartItemProps {
-    id: number;
-    product: Product;
-    quantity: number;
-    isCopy: boolean;
+    cartItem: CartItemDTORecord;
+    removeCartItem: (cartItemId: number) => void;
+    addCartItem: (cartItem: CartItemDTORecord) => void;
 }
 
-function CartItem({ product,/*quantity,*/isCopy }: cartItemProps) {
+function CartItem(props: cartItemProps) {
 
-    /*
-    const price= () =>{
-        if(isPainting(product)){
-            
-            if(!isCopy){
-                return `Price: ${product.price}.00MXN`
-            }else{
-                return `Price: ${product.price_copy}.00MXN`
-            }
-            
-        }else{
-            return  `Price: ${product.price}.00MXN`
-        }
+   
+    let paintingOriginalDiv;
+    if (props.cartItem.details.productType == ProductTypeEnum.PAINTING) {
+        const paintingItemDetails = props.cartItem.details as PaintingItemDetails;
+
+        const paintingInfo = <div className="option ">
+            {paintingItemDetails.isOriginalSelected ? "Original" : "Serialized copy"}
+        </div>
+
+        paintingOriginalDiv = props.cartItem.details.productType == ProductTypeEnum.PAINTING ? paintingInfo : <div></div>
+
     }
-    */
 
 
-
-    const paintingOriginalDiv = (isPainting(product)) ? <div className="option ">{(<div><span className={`cursor-pointer ${isCopy ? "" : "text-blue-600 underline underline-offset-2"} `}>Original</span> / <span className={`cursor-pointer ${isCopy ? "text-blue-600 underline underline-offset-2" : ""}`} >Serialized print</span></div>)}
-    </div> : <div></div>
 
     return <div className="flex flex-col  md:flex-row min-h-52 md:flex-grow lg:justify-start  h-fit    rounded-lg overflow-hidden shadow-lg ">
         <div className="1st-row w-full  md:h-full md:w-1/4 lg:w-1/6 max-w-1/3  object-contain">
-            <img src={product.images[0].url} alt="" className="h-full w-full  " />
+            <img src={props.cartItem.imageUrl} alt="" className="h-full w-full  " />
         </div>
         <div className=" 2nd-row w-3/5  flex flex-col  justify-between lg:justify-start   py-3 px-2 ">
             <div className="text-space p-2">
-                <div className="product-title text-xl ">{product.name}</div>
+                <div className="product-title text-xl ">{props.cartItem.productName}</div>
                 {paintingOriginalDiv}
                 <div className="available-tag">Available</div>
             </div>
@@ -50,7 +42,7 @@ function CartItem({ product,/*quantity,*/isCopy }: cartItemProps) {
 
             </div>
             <div className="pieces flex flex-row px-2 justify-center  items-end  gap-8  ">
-                <PiecesSelector className="" />
+                <PiecesSelector className="" cartItem={props.cartItem} pieces={props.cartItem.details.itemQuantity} addCartItem={props.addCartItem} removeCartItem={props.removeCartItem} />
                 <Button rounded primary className=" py-0 px-0 h-8 flex w-fit text-nowrap "  > Pay apart </Button>
             </div>
 
@@ -61,7 +53,8 @@ function CartItem({ product,/*quantity,*/isCopy }: cartItemProps) {
     {price()}
     </div>
     </div>*/}
-        <PriceTag productPricing={product.productPricing} />
+        {/*<PriceTag productPricing={cartItem.details} /> */}
+        <div>Total: {props.cartItem.totalPrice}</div>
     </div>
 }
 
